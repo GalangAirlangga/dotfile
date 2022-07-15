f6fe678435062db54aa6b750cbe30db54016ce6a
Galang Airlangga <galangairlangga1@gmail.com>
2022-07-15 15:31:41 +0700

    update install_wsl.sh

--- install_wsl.sh
+++ install_wsl.sh
@@ -49,7 +49,10 @@ sudo apt-get install libnss3-tools jq xsel
 
 echo '-------------------------------------------------'
 echo "installing composer"
-sudo apt install composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
 
 echo '-------------------------------------------------'
 echo "installing valet"
@@ -61,7 +64,7 @@ valet restart
 
 echo '-------------------------------------------------'
 echo "installing mysql"
-sudo apt install mysql-server -y
+sudo apt-get install mariadb-server mariadb-client
 sudo mysql -u root -proot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';"
 
 echo '-------------------------------------------------'
